09bb83ef899b8cc8de248942e93f5740ae9ad0c9
stshayna@gmail.com <stshayna@gmail.com>
2022-04-05 21:32:03 -0400

    controller & routes done 5 Apr 2022

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,11 @@
+class GamesController < ApplicationController
+  def new
+    @letters = []
+    10.times do
+      @letters << [*('A'..'Z')].sample
+    end
+    @letters
+  end
+
+  def score; end
+end
